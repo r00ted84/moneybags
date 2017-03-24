66e640e560341440f58688a8b725cc46709078ba
Bobby Estrada <bobbyestrada@Bobbys-Mac-mini.local>
2017-03-23 19:18:25 -0700

    First Commit

---
+++ moneybags/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  moneybags
+//
+//  Created by Bobby Estrada on 3/23/17.
+//  Copyright © 2017 Bobby Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Money, Bags")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
